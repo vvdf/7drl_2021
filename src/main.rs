a50292899c963c7a89a4d68379102acba4e79229
vvdf <t42please@gmail.com>
2021-03-12 00:01:23 -0800

    ADD basic sim term render

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("7DRL, woo!");
+use rltk::{ Rltk, GameState };
+
+struct State {}
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+        ctx.print(1, 1, "Howdy Doody");
+    }
+}
+
+fn main() -> rltk::BError {
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50()
+        .with_title("CHESS NUTS, 7DRL 2021")
+        .build()?;
+    let gs = State{ };
+    rltk::main_loop(context, gs)
 }
