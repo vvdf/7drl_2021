a29d890050ba49f680896333c4209633ef91f13e
vvdf <t42please@gmail.com>
2021-04-07 23:55:36 -0700

    ADD basic player input handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use rltk::{ Rltk, GameState, RGB,/* VirtualKeyCode */};
+use rltk::{ Rltk, GameState, RGB, VirtualKeyCode };
 use specs::prelude::*;  // ecs system
 use specs_derive::Component; // ecs component
-// use std::cmp::{ max, min };
+use std::cmp::{ max, min };
 
 #[derive(Component)] // contains component storage
 struct Position {
@@ -26,10 +26,34 @@ struct State {
     ecs: World,
 }
 
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min(79, max(0, pos.x + delta_x));
+        pos.y = min(49, max(0, pos.y + delta_y));
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    match ctx.key {
+        None => {}
+        Some(key) => match key {
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
+        },
+    }
+}
+
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
 
+        player_input(self, ctx);
         self.run_systems();
 
         let positions = self.ecs.read_storage::<Position>();
